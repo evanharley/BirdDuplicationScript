4ea4fd115e0c566fc4bb18fef06e9455e41a3af9
evharley <eharley@royalbcmuseum.bc.ca>
2019-02-28 07:05:38 -0800

    Fleshing out of parser

--- BirdDuplicationScript/BirdDuplicationScript.py
+++ BirdDuplicationScript/BirdDuplicationScript.py
@@ -1,33 +1,67 @@
 import openpyxl
+import os
 import tkinter
-
+from tkinter import filedialog
 
 class BirdDuplicator:
 
     def __init__(self, *args, **kwargs):
-        root = tkinter()
-        self.input_folder = file_dialog.GetDirectory()
+        file_name = filedialog.askopenfilename(title='Select the Item data Spreadsheet',
+                                         filetypes=[('xlsx files', '.xlsx')])
+        head, tail = os.path.split(file_name)
+        self.input_folder = head
+        self.filename = tail
         self.output_folder = self.input_folder + '\\new_versions\\'
-        self.file = openpyxl.load_workbook(file_dialog.GetPath)
+        self.file = openpyxl.load_workbook(file_name)
+        self.ws = self.file['OrnithologyItem']
         self.item_ids = self._create_data_dictionary()
         return super().__init__(*args, **kwargs)
+
+    def set_file_name(file_name):
+        head,tail = os.path.split(file_name)
+        self.filename = tail
 
     def parse_spreadsheet(self):
         # takes each of the rows of the spreadsheet and does some logic to inform which helper
         # methods need to be run to handle the row
-        print('stuff')
+        last_row = self.ws.max_row
+        for sheet in self.file.sheetnames:
+            self.ws = self.file[sheet]
+            if self.ws[1][1] != 'item_id':
+                continue
+            for row in range(2, last_row + 1):
+                row_data = [self.ws[row][i] for i in range(self.ws.max_column)]
+                if row_data[1] in self.item_ids.keys():
+                    for item in self.item_ids[row_data[1]]:
+                        new_row = row_data
+                        new_row[1] = item
+                        self.ws.append(new_row)
+
+        self.file.save('{0}\\{1}'.format(self.output_folder, self.filename))
         return 0
 
     def _create_data_dictionary(self):
         # Takes the Item Spreadsheet and generates a dictionary in the form
         # {item_id: [ids for the component records]}
-
-        return 0
-
-    def _duplicate_record(self, row):
-        # Main method for duplication of records
-        return 0
+        data_dictionary = {}
+        keys = {self.ws[1][col].value: col for col in range(0, self.ws.max_column)}
+        max_id = sorted([self.ws[row][keys['item_id']].value for row in range(2, self.ws.max_row + 1)])[-1]
+        for row in range(2, self.ws.max_row + 1):
+            item_id = self.ws[row][keys['item_id']].value
+            specimen_nature = self.ws[row][keys['specimen_nature']].value.split('; ')
+            data_dictionary[item_id] = [max_id + i for i in range(1, len(specimen_nature) + 1)]
+            max_id += len(specimen_nature)
+                
+        return data_dictionary
 
     def duplicate_birds(self):
         # Main method for duplication of all records
+        for dirpath, dirname, filename in os.walk(self.input_folder):
+            ignore = ['BIRD', 'RBCM']
+            if any(set_file_name.startswith(item) for item in ignore):
+                continue
+            self.file = openpyxl.load_workbook(filename)
+            self.set_file_name(filename)
+            self.parse_spreadsheet()
+
         return 0
--- BirdDuplicationScript/TestBirdDuplication.py
+++ BirdDuplicationScript/TestBirdDuplication.py
@@ -13,11 +13,15 @@ class Test_test1(unittest.TestCase):
     def test_data_dictionary(self):
         self.dupe._create_data_dictionary()
         test_values = self.dupe.item_ids
-        values = {800002:[821356],
-                  816865: [821357, 821358, 821359],
-                  820109: [821360, 821361, 821362],
-                  821535:[821363, 821364]}
+        values = {800002:[821536],
+                  816865: [821537, 821538, 821539],
+                  820109: [821540, 821541, 821542],
+                  821535:[821543, 821544]}
         self.assertEqual(test_values, values)
+
+    def test_parse_spreadsheet(self):
+        self.dupe.parse_spreadsheet()
+
 
 if __name__ == '__main__':
     unittest.main()
